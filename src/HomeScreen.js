43249853f57db37c3b1f62d0f1eaa933a47ff6a5
WebApp07 <aminezouguig@proton.me>
2024-03-24 04:26:42 -0700

    Building the Row components

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -2,12 +2,26 @@ import React from "react";
 import "./HomeScreen.css";
 import Nav from "./Nav";
 import Banner from "./Banner";
+import requests from "./Request";
+import Row from "./Row";
 
 const HomeScreen = () => {
   return (
     <div className="homeScreen">
       <Nav />
       <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTreding} />
+      <Row title="Top Rated" fetchUrl={requests.fetchToRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romace Movies" fetchUrl={requests.fetchRomaceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 };

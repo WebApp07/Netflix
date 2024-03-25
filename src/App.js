5bec82412c8155dff6a4dfc549d8e0521c2a5159
WebApp07 <aminezouguig@proton.me>
2024-03-25 06:52:25 -0700

    Building the SignupScreen Component with all functionality firebase auth login signup

--- src/App.js
+++ src/App.js
@@ -2,9 +2,24 @@ import "./App.css";
 import HomeScreen from "./screens/HomeScreen";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginScreen from "./screens/LoginScreen";
+import { useEffect } from "react";
+import { auth } from "firebase";
+import firebase from "firebase";
 
 const App = () => {
   const user = null;
+
+  useEffect(() => {
+    const unsubscribe = firebase.auth().onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        console.log(userAuth);
+      } else {
+      }
+    });
+
+    return unsubscribe;
+  }, []);
+
   return (
     <div className="app">
       <Router>
--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { useRef } from "react";
+import "./SignUpScreen.css";
+import { auth } from "../firebase";
 
 const SignUpScreen = () => {
-  return <div className="signupScreen">SignUpScreen</div>;
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  const register = (e) => {
+    e.preventDefault();
+
+    auth
+      .createUserWithEmailAndPassword(
+        emailRef.current.value,
+        passwordRef.current.value
+      )
+      .then((authUser) => {
+        console.log(authUser);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+
+  const signIn = (e) => {
+    e.preventDefault();
+    auth
+      .signInWithEmailAndPassword(
+        emailRef.current.value,
+        passwordRef.current.value
+      )
+      .then((authUser) => {
+        console.log(authUser);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+
+  return (
+    <div className="signupScreen">
+      <from>
+        <h1>Sign In</h1>
+        <input ref={emailRef} placeholder="Email" type="email" />
+        <input ref={passwordRef} placeholder="Password" type="password" />
+        <button type="submit" onClick={signIn}>
+          Sign In
+        </button>
+
+        <h4>
+          <span className="signupScreen__gray">New to Netflix? </span>
+          <span className="signupScreen__link" onClick={register}>
+            Sign Up now.
+          </span>
+        </h4>
+      </from>
+    </div>
+  );
 };
 
 export default SignUpScreen;
